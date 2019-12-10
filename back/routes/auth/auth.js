a74a0023d6b3896f2a22df1cb088ff0011dc6e49
Retno <retnoscwulandari@gmail.com>
2019-12-10 15:53:13 +0000

    npm_retno_quest040

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const connection = require("../../helpers/db.js");
 
 let router = express.Router();
 
 router.post("/signup", function(req, res, next) {
-  res.send("I am in POST signup");
+  let registrationData = req.body;
+  connection.query(
+    "INSERT INTO users SET ?",
+    registrationData,
+    (error, result) => {
+      if (error) {
+        console.log(error);
+        res.status(500).end();
+        res.end();
+      } else {
+        console.log(result);
+        res.send("Your form has been submitted!");
+      }
+    }
+  );
 });
 
 module.exports = router;
